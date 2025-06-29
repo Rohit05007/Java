3d7eef4c77f040940edb190b218738e2d2a9de6e
Rohit <rohitsingh200705@gmail.com>
2025-06-29 08:37:08 +0530

    practice_Q1.java

--- practice_Q1.java
+++ practice_Q1.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 class practice_Q1 {
     public static void main(String[] args) {
-        // Question 2---------------------
-        // System.out.print("Enter the name :");
-        // Scanner sc=new Scanner(System.in);
-        // String name =sc.nextLine();
-        // System.out.println("Hello "+name+" have a good day !");
-  // Question 3------------------------
+/* Question 1 Writw a java program which asks the user to enter his name and greets them with
+     "hello <name >,have a good day "text ---------------------*/
+        System.out.print("Enter the name :");
+        Scanner sc=new Scanner(System.in);
+        String name =sc.nextLine();
+        System.out.println("Hello "+name+" have a good day !");
+// Question 2 WAP of java to convert kilometers to miles------------------------
 System.out.print("Enter the number to convert kilometers in miles :");
 Scanner sc = new Scanner(System.in);
 float kilometers =sc.nextFloat();
