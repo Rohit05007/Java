f2a9af68126d20b2c1e198fb71d07c5d8374fd08
Rohit <rohitsingh200705@gmail.com>
2025-06-29 08:31:37 +0530

    Update chapter_1.java

--- chapter_1.java
+++ chapter_1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 class chapter_1 {
     public static void main(String[] args) {
-        //Arithmetic Operator 
+        // Arithmetic Operator
         int a = 4;
         // int b=5 %a; // modulo operator
         int b = 5;
@@ -12,24 +12,12 @@ class chapter_1 {
         System.out.println(b);
 
         // Comparion Operator
-        System.out.println(6 == 8); 
-        System.out.println(6 < 8); 
+        System.out.println(6 == 8);
+        System.out.println(6 < 8);
 
         // Logical Operator
-        System.out.println(64 > 5 && 64 > 8); 
-        System.out.println(64 > 5 || 64 > 98); 
-
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println(64 > 5 && 64 > 8);
+        System.out.println(64 > 5 || 64 > 98);
 
     }
 }
