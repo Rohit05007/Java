9bbfdbc0d5693ecd299338b3023c8f52594b1453
Rohit <rohitsingh200705@gmail.com>
2025-07-04 21:24:14 +0530

    Add files via upload

---
+++ v13_strings.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class v13_strings {
+    public static void main(String[] args) {
+        // String name = new String("Rohit");
+        String name = "Rohit";
+        System.out.print("The name is :");
+        System.out.println(name);
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("the value of a is %d and value of b is %f  ", a, b);
+        // %d = int is format Specifier
+        // %f for float
+        // %c for char
+        // %s for string
+        Scanner sc = new Scanner(System.in);
+        String st =sc.next();
+System.out.println(st);
+
+    }
+}
