e9dc82d50e50918123e359716d306d30c67c201a
Rohit <rohitsingh200705@gmail.com>
2025-07-09 19:45:05 +0530

    Update chapter_2_Practice_Set_v13.java

--- chapter_2_Practice_Set_v13.java
+++ chapter_2_Practice_Set_v13.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class chapter_2_Practice_Set_v13{
+class chapter_2_Practice_Set_v13 {
     public static void main(String[] args) {
         // Problem 1
         String name = "ROHIT";
@@ -12,26 +12,20 @@ class chapter_2_Practice_Set_v13{
         System.out.println(text);
 
         // Problem 3
-       String letter="Dear Rohit , Thanks a lot";
-       System.out.println(letter);
-       letter=letter.replace("Rohit", "Rohan");
-       System.out.println(letter);
+        String letter = "Dear Rohit , Thanks a lot";
+        System.out.println(letter);
+        letter = letter.replace("Rohit", "Rohan");
+        System.out.println(letter);
 
-//Problem 4
-String mystring="This string contains  double and   triple spaces";
- System.out.println(mystring.indexOf("  "));//;If this give postive value this mean in this string positive value present .
-System.out.println(mystring.indexOf("   "));// If this give postive value this mean in this string positive value present .
-System.out.println(mystring.indexOf("    "));//If this give negitive value this mean in this string four space not present.
+        // Problem 4
+        String mystring = "This string contains  double and   triple spaces";
+        System.out.println(mystring.indexOf("  "));
+        System.out.println(mystring.indexOf("   "));
+        System.out.println(mystring.indexOf("    "));
 
-//Problem 5
-String myletter = " Dear ,\n\t This java course is nice.\n\tThank!";
-System.out.println(myletter);
+        // Problem 5
+        String myletter = " Dear ,\n\t This java course is nice.\n\tThank!";
+        System.out.println(myletter);
 
-
-
-
-
-
-
-}
+    }
 }
